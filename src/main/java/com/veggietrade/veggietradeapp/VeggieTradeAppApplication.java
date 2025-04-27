1976e200c8738e10c17fb9ee09826f1a8aa84afa
Girish Dondapati <girishdondapati10@gmail.com>
2025-04-27 21:38:11 +0530

    Initial commit for veggie-trade-app

---
+++ src/main/java/com/veggietrade/veggietradeapp/VeggieTradeAppApplication.java
@@ -0,0 +1,13 @@
+package com.veggietrade.veggietradeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeggieTradeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeggieTradeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veggietrade/veggietradeapp/VeggieTradeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veggietrade.veggietradeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeggieTradeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
